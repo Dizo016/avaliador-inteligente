e4e7cb8abc4422870324e15ddc1f8d7c7dfd6465
Diego Silva <diegossilva2008@gmail.com>
2025-04-02 23:30:17 -0300

    Estrutura inicial do projeto criada

---
+++ src/main/java/com/ucsal/avaliador_inteligente/AvaliadorInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.ucsal.avaliador_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucsal/avaliador_inteligente/AvaliadorInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucsal.avaliador_inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliadorInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
